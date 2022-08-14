74b64cb803b063012e5599dc8a33cbdff2ae611b
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-08-14 23:54:42 +0200

    Port setup

---
+++ CarRentalET/Controllers/CarRentalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarRentalController : ControllerBase
+    {
+        [HttpGet("GetCars")]
+        public IActionResult Get()
+        {
+            return Ok(1);
+        }
+    }
+}
