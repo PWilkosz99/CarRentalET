7a929511587f7802b864b188aadcf2680a6e5cc5
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-10-21 14:10:01 +0200

    Get data from api

--- CarRental.Frontend/src/App.js
+++ CarRental.Frontend/src/App.js
@@ -4,13 +4,27 @@ import './App.css';
 
 function App() {
 
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    fetch(`https://localhost:5001/api/CarRental/GetCars`)
-      .then((response) => console.log(response));
+    async function getData() {
+      const res = await fetch('https://localhost:5001/api/CarRental/GetCars');
+      const data = await res.json();
+      setData(data);
+      console.log(data);
+    }
+    getData();
   }, []);
+
+  var cars = data?.map((car) => {
+    return (
+      <h1>{car.mileage}</h1>
+    )
+  })
 
   return (
     <div className="App">
+      {cars}
     </div>
   );
 }
