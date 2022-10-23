9a7bf171681e64c5550523f74f0f7682ff9a7e89
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-10-24 01:45:54 +0200

    Login status

--- CarRental.Frontend/src/components/Home.jsx
+++ CarRental.Frontend/src/components/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
-    console.log("home");
-  return(
-    <h2>Home page</h2>
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    (
+      async () => {
+        const responde = await fetch('http://localhost:5000/auth/user', {
+          headers: { 'Content-Type': 'application/json' },
+          credentials: 'include'
+        });
+
+        const content = await responde.json();
+        setName(content.name);
+
+      }
+    )();
+  });
+
+  return (
+    <>
+        <h2>Home page</h2>
+        {name ? 'Zalogowano jako ' + name : 'Nie zalogowano'}
+    </>
   );
 }
