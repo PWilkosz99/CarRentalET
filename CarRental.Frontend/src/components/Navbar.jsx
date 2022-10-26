f7678d29ab6d31f072ae2bab160a4420dcf7374b
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-10-26 23:33:27 +0200

    React icons

--- CarRental.Frontend/src/components/Navbar.jsx
+++ CarRental.Frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoCarSport } from "react-icons/io5";
 import '../App.css';
 
 export default function Navbar(props) {
@@ -16,7 +17,8 @@ export default function Navbar(props) {
 
     return (
         <nav>
-            <img src="logo192.png" className="nav-logo" />
+            {/* <img src="logo192.png" className="nav-logo" /> */}
+            <IoCarSport color="black" size="5em"/>
             <ul className="nav-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/dashboard">Dashboard</Link></li>
