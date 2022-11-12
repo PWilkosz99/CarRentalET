f92fb228177f9814496d6eb498a0d6f2b16f250d
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-11-12 17:11:33 +0100

    Swagger

--- CarRental.Backend/Program.cs
+++ CarRental.Backend/Program.cs
@@ -10,9 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<CarRentalContext>(
-    o => o.UseNpgsql(builder.Configuration.GetConnectionString("CarRentalDb"))
-    );
+builder.Services.AddDbContext<CarRentalContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("CarRentalDb")));
 
 builder.Services.AddCors(options =>
     options.AddPolicy(
@@ -36,16 +34,19 @@ builder.Services
         };
     });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 //builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<JwtService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 //app.UseHttpsRedirection();
@@ -58,11 +59,6 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
+app.MapControllers();
 
 app.Run();
