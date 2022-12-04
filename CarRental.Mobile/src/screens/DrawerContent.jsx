c29a052cc311a7c126f264f137900b342913cd53
Piotr Wilkosz <pwilkosz99@gmail.com>
2022-12-05 00:20:07 +0100

    Drawer icons

--- CarRental.Mobile/src/screens/DrawerContent.jsx
+++ CarRental.Mobile/src/screens/DrawerContent.jsx
@@ -4,20 +4,51 @@ import React from 'react'
 import HomeScreen from './HomeScreen';
 import TestScreen from './TestScreen';
 import CustomDrawer from './CustomDrawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const DrawerContent = () => {
     const Drawer = createDrawerNavigator();
     return (
-        <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
-            <Drawer.Screen name="Home" component={HomeScreen} />
-            <Drawer.Screen name="Reservations" component={HomeScreen} />
-            <Drawer.Screen name="Rent" component={HomeScreen} />
-            <Drawer.Screen name="Test" component={TestScreen} />
-            <Drawer.Screen name="Logout" component={TestScreen} />
+        <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}
+        screenOptions={{headerShown: true, drawerLabelStyle: styles.drawerLabel, drawerActiveTintColor: 'blue'}}>
+            <Drawer.Screen name="Home" component={HomeScreen} options={{
+                drawerIcon: ({ color }) => (
+                    <Icon name="home-outline" style={styles.icon} color={color} />
+                )
+            }} />
+            <Drawer.Screen name="Reservations" component={HomeScreen} options={{
+                drawerIcon: ({ color }) => (
+                    <Icon name="calendar-outline" style={styles.icon} color={color} />
+                )
+            }} />
+            <Drawer.Screen name="Rent" component={HomeScreen} options={{
+                drawerIcon: ({ color }) => (
+                    <Icon name="car-outline" style={styles.icon} color={color} />
+                )
+            }} />
+            <Drawer.Screen name="Test" component={TestScreen} options={{
+                drawerIcon: ({ color }) => (
+                    <Icon name="balloon-outline" style={styles.icon} color={color} />
+                )
+            }} />
+            <Drawer.Screen name="Logout" component={TestScreen} options={{
+                drawerIcon: ({ color }) => (
+                    <Icon name="home-outline" style={styles.icon} color={color} />
+                )
+            }} />
         </Drawer.Navigator>
     )
 }
 
 export default DrawerContent
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    icon: {
+        fontSize: 22
+    },
+    drawerLabel: {
+        marginLeft: 5,
+        fontSize: 18,
+        fontFamily: 'Roboto-Medium',
+    }
+})
